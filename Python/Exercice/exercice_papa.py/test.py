038fed078f910f11e08ededb6d846ac746ac9ca3
Gauthier-R <gauthier.raimbault@gmail.com>
2024-12-23 10:03:06 +0100

    Retirer le fichier volumineux du commit précédent

---
+++ Python/Exercice/exercice_papa.py/test.py
@@ -0,0 +1,16 @@
+import customtkinter
+
+app = customtkinter.CTk()
+
+def checkbox_event():
+    print("checkbox toggled, current value:", check_var.get())
+
+check_var = customtkinter.StringVar(value="on")
+checkbox = customtkinter.CTkCheckBox(app, text="CTkCheckBox", command=checkbox_event,
+                                     variable=check_var, onvalue="on", offvalue="off")
+
+checkbox.pack()
+
+app.mainloop()
+
+
